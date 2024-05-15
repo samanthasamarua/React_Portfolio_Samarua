3d4e7a63ea688353d32745559e5078dea7df1cf5
Samantha Samarua <samantha.samarua@gmail.com>
2024-05-16 03:41:37 +0930

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   server: {
     port: 3002,
-    open: true
-  }
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+  },
 });
